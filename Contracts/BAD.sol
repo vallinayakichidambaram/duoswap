01bb2aaba5655d3ee5c515e32e6bd921f37768c7
vallinayakichidambaram <acgvalli@gmail.com>
2024-03-14 17:11:23 +0530

    added token and swap contracts

---
+++ Contracts/BAD.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BAD is ERC20 {
+    constructor() ERC20("Badri", "BAD") {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+}
