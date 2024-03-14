48e7dc9f238282a95c4ae346f93fe40619670fff
vallinayakichidambaram <acgvalli@gmail.com>
2024-03-14 19:33:48 +0530

    plan

---
+++ createAccount.js
@@ -0,0 +1,4 @@
+//Create admin account and store it in env file
+//Env file contents - Admin public address, private key and infura/alchemy url for Avalanche testnet
+
+//Once token is deployed, store the address of the deployed token in the env file -> from there take it and pass it to swap contract
---
+++ deploy.js
@@ -0,0 +1,5 @@
+const baaTokenAbi = require('bin/BAA.json')
+const badTokenAbi = require('bin/BAD.json')
+
+
+//Create Admin account and deploy the contract using abi
