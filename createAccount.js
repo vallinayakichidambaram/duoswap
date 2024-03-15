b02f33e12cf3bbaa7396d413d9f1522baf4fb0ea
vallinayakichidambaram <acgvalli@gmail.com>
2024-03-15 14:30:35 +0530

    admin wallet creation

--- createAccount.js
+++ createAccount.js
@@ -2,3 +2,20 @@
 //Env file contents - Admin public address, private key and infura/alchemy url for Avalanche testnet
 
 //Once token is deployed, store the address of the deployed token in the env file -> from there take it and pass it to swap contract
+
+
+//rpc - https://avalanche-fuji-c-chain-rpc.publicnode.com	
+//wss - wss://avalanche-fuji-c-chain-rpc.publicnode.com	
+// Faucet - https://core.app/tools/testnet-faucet/?subnet=c&token=c
+const { Web3 } = require('web3')
+const Accounts = require('web3-eth-accounts');
+async function createAdminAccount () {
+    const web3 = new Web3('https://subnets.avax.network/wagmi/wagmi-chain-testnet/rpc');
+    const adminAccount = web3.eth.accounts.create();
+    console.log(`Admin Account=====> ${adminAccount.address}`);
+    console.log(`Admin Private Key=====> ${adminAccount.privateKey}`);
+
+}
+
+
+createAdminAccount()
